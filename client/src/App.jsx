a50979566576bfc3bdc7fa121e53bdb6b630584a
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2025-08-25 16:13:31 +0800

    Frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-black-200">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        budgET 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
